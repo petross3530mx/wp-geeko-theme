bdaccf223b158ac3cd7b46a2ba1ce40d6265c9eb
Petro Petro <petross3530mx@gmail.com>
2020-11-28 09:51:44 +0200

    flat range logic update

--- job_manager/job-maindetails.php
+++ job_manager/job-maindetails.php
@@ -46,17 +46,22 @@ global $user_role;
 			else{?>
 				<?php echo get_field('salary_r1') ?? get_field('salary_r2');?>
 			<?php } ?>
-	    </div>
-	    <div class="detail-item">
-	        <p>Split %</p>
-	        <?php if($val1 && $val2){ ?>
-				<small class="small-green">$<?php echo intval($val1/2);  ?> - $<?php echo intval($val2/2);  ?></small>
-			<? } ?>
-	    </div>
-	    <div class="detail-item">
-	        <p>Flat Split</p>
-	        <small class="small-green">$<?php number_format(intval(the_field('flat_range')), 0, '', ' '); ?></small>
-	    </div>
+		</div>
+		<?php $flat_split = get_field('flat_range'); ?>
+		<? if(intval($flat_split)){ ?>
+			<div class="detail-item">
+				<p>Flat Split</p>
+				<small class="small-green">$<?php number_format(intval(the_field('flat_range')), 0, '', ' '); ?></small>
+			</div>
+		<?php }
+		else{ ?>
+			<div class="detail-item">
+				<p>Split %</p>
+				<?php if($val1 && $val2){ ?>
+					<small class="small-green">$<?php echo intval($val1/2);  ?> - $<?php echo intval($val2/2);  ?></small>
+				<? } ?>
+			</div>
+		<?php } ?>
 	</div>
 <?php }
 else{ ?>
@@ -70,14 +75,26 @@ else{ ?>
 	        <p>Type</p>
 	        <small><?php echo $datailinfo['job_listing_type']; ?></small>
 	    </div>
-	    <?php endif; ?>
-	    <div class="detail-item">
-	        <p>Split %</p>
-	        <small  class="small-green">15%</small>
-	    </div>
-	    <div class="detail-item">
-	        <p>Flat Split</p>
-	        <small  class="small-green">$15 000</small>
-	    </div>
+		<?php endif; ?>
+		
+		<?php $val1 = intval(preg_replace('/[^0-9]/', '', $salary1)) * $fee / 100; ?> </small>   - 
+		<?php $val2 = intval(preg_replace('/[^0-9]/', '', $salary2)) * $fee / 100; ?></small> 
+
+
+	    <?php $flat_split = get_field('flat_range'); ?>
+		<? if(intval($flat_split)){ ?>
+			<div class="detail-item">
+				<p>Flat Split</p>
+				<small class="small-green">$<?php number_format(intval(the_field('flat_range')), 0, '', ' '); ?></small>
+			</div>
+		<?php }
+		else{ ?>
+			<div class="detail-item">
+				<p>Split %</p>
+				<?php if($val1 && $val2){ ?>
+					<small class="small-green">$<?php echo intval($val1/2);  ?> - $<?php echo intval($val2/2);  ?></small>
+				<? } ?>
+			</div>
+		<?php } ?>
 	</div>
 <?php } ?>
