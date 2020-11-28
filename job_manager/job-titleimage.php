5b2b535c126463ad24059a20bb531a3a9048a8d7
Petro Petro <petross3530mx@gmail.com>
2020-11-28 09:27:50 +0200

    logo update

--- job_manager/job-titleimage.php
+++ job_manager/job-titleimage.php
@@ -1,9 +1,32 @@
+<?php $logo = get_field('jobs_logo'); ?>
+<?php $current_post_author_id = get_the_author_meta( 'ID' );  ?>
+<?php $company_name = get_the_author_meta('display_name', $current_post_author_id); ?>
+
+<?php if(!empty($logo)){ ?>
+
 <div class="jt job-imgx">
-    <?php the_company_logo(); ?>
+    <img src="<?php echo $logo; ?>" alt="">
 </div>
+
+<? }
+else{ ?>
+<div class="jt job-imgx name-char">
+    
+    <?php if (strpos($company_name, ' ') !== false) {
+        $parts = explode( ' ', $company_name);
+        foreach ($parts as $part){
+            echo substr($part, 0, 1);  
+        }
+    }
+    else{
+        echo substr($company_name, 0, 1);  
+    } ?>
+</div>
+<? } ?>
+
 
 <div class="jx">
     <h1><?php the_title();?></h1>
-    <?php $current_post_author_id = get_the_author_meta( 'ID' );  ?>
-    <p><?php echo get_the_author_meta('display_name', $current_post_author_id);?></p>
+    
+    <p><?php echo $company_name; ?></p>
 </div>
